d6811642dfaaa7f1cd9e9bf42c94ef04d9495e9a
Nozima <nozimaqodirova07@gmail.com>
2025-03-17 01:03:11 +0500

    done

--- index.ts
+++ index.ts
@@ -18,6 +18,36 @@
     
 }
 {
+    //masala2
+    class BankAccount {
+                private balance: number = 0
+                    constructor(balance: number){
+                        this.balance = balance
+                    }
+                    getBalance(){
+                        return this.balance
+                    }
+                deposit(amount: number){
+                    this.balance += amount
+                }
+                pay(amount: number){
+                    if(amount > this.balance){
+                        return "You do not have enough money for that mission"
+                    }
+                    this.balance -= amount
+                }
+            }
+        
+            const owner: BankAccount = new BankAccount(0)
+        
+            owner.deposit(100)
+        
+            owner.pay(49)
+        
+            console.log(owner.getBalance())
+}
+    
+{
     //masala3
     class Circle{
         radius:number
@@ -33,6 +63,30 @@
      const radius = new Circle( 3.1416, 5)
      console.log(radius.getArea());
      
+}
+{
+    //masala4
+    class Product {
+            name : string
+            price: number
+            constructor(price: number) {
+              this.price = price;
+            }
+            getBalance() {
+              return this.price;
+            }
+        
+            applayDiscount(percentage: number) {
+              
+              this.price = this.price - ( this.price * percentage / 100)
+            }
+          }
+        
+          const owner: Product = new Product(500);
+        
+          owner.applayDiscount(20);
+        
+          console.log(owner.getBalance());
 }
 {
     //masala5
